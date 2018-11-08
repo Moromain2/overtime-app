4cf08b7fddbaba7fe9dcbfb8e7817a5267cd05db
Romain Mosser <mosser.romain@gmail.com>
2018-11-08 18:52:46 +1000

    fixed issue with the STI admin user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creation" do
+    it "can be created" do
+      user = User.create(email: "test@test.com", password: "qsdfqsdf", password_confirmation: "qsdfqsdf")
+    end
+  end
 end
